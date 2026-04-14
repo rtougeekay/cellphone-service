89fa7d42f8d6015182382ff4217a56d6ed570adf
J Richard K Tougeekay <rtougeekay@gmail.com>
2026-04-14 11:24:35 -0500

    Cellphone exercise

---
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+public class CellPhoneApplication {
+        private int serialNumber;
+        private String modelNumber;
+        private String carrier;
+        private String phoneNumber;
+        private String owner;
+
+        public CellPhoneApplication(){
+            serialNumber = 0;
+            modelNumber = "";
+            carrier = "";
+            phoneNumber = "";
+            owner = "";
+        }
+public int getSerialNumber(){
+            return serialNumber;
+}
+
+
+
+
+}
