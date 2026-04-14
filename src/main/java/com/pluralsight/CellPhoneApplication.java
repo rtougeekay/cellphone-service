2588a0aa346e990e1b488478a79623a1363acf41
J Richard K Tougeekay <rtougeekay@gmail.com>
2026-04-14 11:33:44 -0500

    Cellphone exercise getters and setters

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,24 +1,57 @@
 package com.pluralsight;
 
 public class CellPhoneApplication {
-        private int serialNumber;
-        private String modelNumber;
-        private String carrier;
-        private String phoneNumber;
-        private String owner;
+    private int serialNumber;
+    private String modelNumber;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
 
-        public CellPhoneApplication(){
-            serialNumber = 0;
-            modelNumber = "";
-            carrier = "";
-            phoneNumber = "";
-            owner = "";
-        }
-public int getSerialNumber(){
-            return serialNumber;
-}
+    public CellPhoneApplication() {
+        serialNumber = 0;
+        modelNumber = "";
+        carrier = "";
+        phoneNumber = "";
+        owner = "";
+    }
 
+    public int getSerialNumber() {
+        return serialNumber;
+    }
 
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
 
+    public String getModelNumber() {
+        return modelNumber;
+    }
 
+    public void setModelNumber(String modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
